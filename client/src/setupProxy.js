7873025dabb3ddfac45674b329715d1ef839d01d
renji18 <aadarshjha1401@gmail.com>
2023-06-01 14:27:39 +0530

    switching distro

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://192.168.29.154:5000',
+      target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
